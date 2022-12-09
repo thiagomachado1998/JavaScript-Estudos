36b7d608199286be4a725e7ef607dd2f23f77ebf
thiagomachado1998 <thiago_machado13@hotmail.com>
2022-12-08 21:40:13 -0300

    Create array.js

---
+++ array/array.js
@@ -0,0 +1,5 @@
+//é uma boa pratica nao misturar diversos tipos de dados difrentes em um mesmo array
+
+let elementos = ['bia','ana','joao']
+elementos.splice(1,2,'thiago','pedro')
+console.log(elementos)
